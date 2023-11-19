a91538909690b46e68f29900580bb24ab8ca9eab
VoQuocHiep <150458559+VoQuocHiep@users.noreply.github.com>
2023-11-19 08:28:04 +0700

    Update Exercise79.py

--- Exercise79.py
+++ Exercise79.py
@@ -1,1 +1,19 @@
+import random
+S = random.randint(1, 100)
+d= 0
+print(f"{S}")
 
+for i in range(99):
+    z = random.randint(1, 100)
+    print(f"{z}",end="")
+    
+    if z > S:
+        S= z
+        d += 1
+        print(" <== Update")
+    else:
+        print()
+
+# Hiển thị kết quả cuối cùng
+print(f"\nThe maximum value found was {S}")
+print(f"The maximum value was updated {d}")
