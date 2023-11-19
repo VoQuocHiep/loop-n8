b11bc7f384aaf277e304df96aa5ba9cc51723f70
VoQuocHiep <150458559+VoQuocHiep@users.noreply.github.com>
2023-11-19 08:24:54 +0700

    Update Exercise76.py

--- Exercise76.py
+++ Exercise76.py
@@ -1,1 +1,14 @@
-
+n = int(input("Enter an integer (2 or greater) : "))  
+s=n     
+if s < 2:
+   print("Enter an integer greater than or equal to 2")
+else:
+    S=[]
+    for factor in range(2, s + 1):
+        while s % factor == 0:
+            S.append(factor)
+            s //= factor
+    
+    print(f"The prime factors of {n} are:")
+    for factor in S:
+     print(factor)
